ff1eacd94589b8b4cfcb80e7544c81e6bb2fa314
djangogeek <laurent.benedetti@gmail.com>
2013-11-19 23:38:52 +0100

    dfgd

---
+++ rails4app/app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+  def index
+  	@title="Welcome ! that's a rails 4 app"
+  end
+
+  def about
+  	@title="Welcome ! that's a rails 4 app"
+  end
+
+  def help
+  @title="Welcome ! that's a rails 4 app"
+  end
+end
